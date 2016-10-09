79ee437923d8ea3670993713a834e4bdd435dd57
jurin-n <jurin.nakano@gmail.com>
2016-10-09 14:15:05 +0900

    edit command.

--- web-framework/flask/docker-for-webserver/centos-nginx/init.sh
+++ web-framework/flask/docker-for-webserver/centos-nginx/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
+cd /opt/myapp
+uwsgi --socket /tmp/uwsgi.sock --module app --callable app --chmod-socket=666 &
+
 nginx -g "daemon off;"
-
-# cd /opt/myapp
-# uwsgi --socket /tmp/uwsgi.sock --module app --callable app --chmod-socket=666
-
-
