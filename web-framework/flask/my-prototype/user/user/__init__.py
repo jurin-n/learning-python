735413498f50209e766fdda845d454d472ba271b
jurin-n <jurin.nakano@gmail.com>
2016-10-10 18:34:03 +0900

    add create_app().

--- web-framework/flask/my-prototype/user/user/__init__.py
+++ web-framework/flask/my-prototype/user/user/__init__.py
@@ -1,2 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db.init_app(app)
+
+    return app
+
+app = create_app()
