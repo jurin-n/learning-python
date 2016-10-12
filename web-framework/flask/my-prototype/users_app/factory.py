c8fe4dc0f120a7dbb795a9f170e6fac0093a154e
jurin-n <jurin.nakano@gmail.com>
2016-10-12 23:01:06 +0900

    change file name views.py to controllers.py.

--- web-framework/flask/my-prototype/users_app/factory.py
+++ web-framework/flask/my-prototype/users_app/factory.py
@@ -9,7 +9,7 @@ def create_app(config_filename):
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/users_app.db'
 
-    from views import resource
+    from controllers import resource
     from models import User
     app.register_blueprint(resource)
     db.init_app(app)
