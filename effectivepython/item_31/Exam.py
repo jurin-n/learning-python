4a3ef751df75e84af1f453fb3f7e200da8997ac9
jurin-n <jurin.nakano@gmail.com>
2016-11-03 15:59:44 +0900

    add item 31 examle codes.

---
+++ effectivepython/item_31/Exam.py
@@ -0,0 +1,38 @@
+class Exam(object):
+    def __init__(self):
+        self._writing_grade = 0
+        self._math_grade = 0
+    
+    @staticmethod
+    def _check_grade(value):
+        if not(0 <= value <= 100):
+            raise ValueError('Grade must be between 0 and 100')
+    
+    @property
+    def writing_grade(self):
+        return self._writing_grade
+    
+    @writing_grade.setter
+    def writing_grade(self, value):
+        self._check_grade(value)
+        self._writing_grade = value
+    
+    @property
+    def math_grade(self):
+        return self._math_grade
+    
+    @math_grade.setter
+    def math_grade(self, value):
+        self._check_grade(value)
+        self._math_grade = value
+
+exam = Exam()
+exam.writing_grade = 40
+print(exam.writing_grade)
+exam.writing_grade = 101
+print(exam.writing_grade)
+
+#exam.math_grade = 40
+#print(exam.math_grade)
+#exam.math_grade = 101
+#print(exam.math_grade)
---
+++ effectivepython/item_31/Homework.py
@@ -0,0 +1,19 @@
+class Homework(object):
+    def __init__(self):
+        self._grade = 0
+    
+    @property
+    def grade(self):
+        return self._grade
+    
+    @grade.setter
+    def grade(self, value):
+        if not(0 <= value <= 100):
+            raise ValueError('Grade must be between 0 and 100')
+        self._grade = value
+
+galileo = Homework()
+galileo.grade = 95
+print(galileo.grade)
+galileo.grade = 101
+print(galileo.grade)
