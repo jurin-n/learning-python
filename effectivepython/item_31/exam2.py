1a3f0550e1b0ce738f359a9632e640e186e6ed98
jurin-n <jurin.nakano@gmail.com>
2016-11-03 19:08:04 +0900

    edit item 31 sample code.

--- effectivepython/item_31/exam2.py
+++ effectivepython/item_31/exam2.py
@@ -7,7 +7,13 @@ class Exam2(object):
     writing_grade = Grade()
     science_grade = Grade()
 
-exam = Exam2()
-exam.writing_grade = 100
-print(exam.writing_grade)
-    
+first_exam = Exam2()
+first_exam.writing_grade = 100
+first_exam.science_grade = 95
+print('first_exam.writing_grade', first_exam.writing_grade)
+print('first_exam.science_grade', first_exam.science_grade)
+   
+second_exam = Exam2()
+second_exam.writing_grade = 55
+print('second_exam.writing_grade', second_exam.writing_grade, 'is right')
+print('first_exam.writing_grade', first_exam.writing_grade, 'is wrong')
