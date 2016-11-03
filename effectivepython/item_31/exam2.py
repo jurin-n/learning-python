c10d47745e5d5305115517dca401be244deaad97
jurin-n <jurin.nakano@gmail.com>
2016-11-03 19:29:35 +0900

    add WeakKeyDictionary in Grade class.

--- effectivepython/item_31/exam2.py
+++ effectivepython/item_31/exam2.py
@@ -15,5 +15,5 @@ print('first_exam.science_grade', first_exam.science_grade)
    
 second_exam = Exam2()
 second_exam.writing_grade = 55
-print('second_exam.writing_grade', second_exam.writing_grade, 'is right')
-print('first_exam.writing_grade', first_exam.writing_grade, 'is wrong')
+print('second_exam.writing_grade', second_exam.writing_grade)
+print('first_exam.writing_grade', first_exam.writing_grade)
--- effectivepython/item_31/grade.py
+++ effectivepython/item_31/grade.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+from weakref import WeakKeyDictionary
+
 class Grade(object):
     def __init__(self):
-        self._value = 0
+        self._value = WeakKeyDictionary()
 
     def __get__(self, instance, instance_type):
-        return self._value
+        if instance is None:
+            return self
+        return self._value.get(instance, 0)
     
     def __set__(self, instance, value):
         if not(0 <= value <= 100):
             raise ValueError('Grade must be between 0 and 100')
-        self._value = value
-
+        print('[DEBUG]', instance)
+        self._value[instance] = value
