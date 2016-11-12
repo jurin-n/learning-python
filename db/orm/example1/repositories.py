98b604d35fe46e9027a1376fccded1aa7d410a2a
jurin-n <jurin.nakano@gmail.com>
2016-11-13 02:12:57 +0900

    add sample codes for add, query.

--- db/orm/example1/repositories.py
+++ db/orm/example1/repositories.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+from domains import User
 
 def addUser(session=None, users=None):
     session.add_all(users) 
+
+def getAllUser(session=None):
+    return session.query(User).order_by(User.id)
+
+def getUser(session=None, name=None):
+    return session.query(User).filter_by(name=name).first()
--- db/orm/example1/repositories_test.py
+++ db/orm/example1/repositories_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase, main
 from domains import Base, User
-from repositories import addUser
+from repositories import addUser, getAllUser, getUser
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import Session, sessionmaker
 
@@ -17,8 +17,25 @@ class RepositoriesTestCase(TestCase):
     def test_add(self):
         with self.session.begin():
             user = User(name='taro', fullname=u'テスト　太郎', password='tarotaro')
-            addUser(self.session,[user])
+            addUser(self.session, [user])
 
+    def test_get_all_users(self):
+        with self.session.begin():
+            user1 = User(name='taro', fullname=u'テスト　太郎', password='tarotaro')
+            user2 = User(name='jiro', fullname=u'テスト　次郎', password='jirojiro')
+            user3 = User(name='sabu', fullname=u'テスト　三郎', password='sabusabu')
+            addUser(self.session, [user1, user2, user3])
+        users = getAllUser(self.session)
+        self.assertEqual(3, users.count())
+
+    def test_get_an_user(self):
+        with self.session.begin():
+            user1 = User(name='taro', fullname=u'テスト　太郎', password='tarotaro')
+            user2 = User(name='jiro', fullname=u'テスト　次郎', password='jirojiro')
+            user3 = User(name='sabu', fullname=u'テスト　三郎', password='sabusabu')
+            addUser(self.session, [user1, user2, user3])
+        user = getUser(self.session, 'jiro')
+        self.assertEqual('jiro', user.name)
 
 if __name__ == '__main__':
     main()
