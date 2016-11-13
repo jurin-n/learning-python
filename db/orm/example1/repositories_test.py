776ed88c2e0192acc26e67ace9c3cf89952d06b4
jurin-n <jurin.nakano@gmail.com>
2016-11-13 15:19:01 +0900

    add sample example codes for Query.

--- db/orm/example1/repositories_test.py
+++ db/orm/example1/repositories_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#from unittest import TestCase, main
+# from unittest import TestCase, main
 import unittest
 from domains import Base, User, Address
 from repositories import addUser, getAllUser, getUser
@@ -7,6 +7,17 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import Session, sessionmaker
 
 class RepositoriesTestCase(unittest.TestCase):
+    # [メモ]テスト単位でセットアップでもいいとおもったけど、テストケース単位での
+    # [メモ]セットアップもそれほど遅くないのでテストケース単位にした。
+    # @classmethod
+    # def setUpClass(cls):
+    #    engine = create_engine('sqlite:///repositories_test.db', echo=False)
+    #    Base.metadata.drop_all(engine)
+    #    Base.metadata.create_all(engine)
+    #    # self.session = Session(engine)
+    #    Session = sessionmaker(bind=engine, autocommit=True)
+    #    session = Session()
+
     def setUp(self):
         engine = create_engine('sqlite:///repositories_test.db', echo=True)
         Base.metadata.drop_all(engine)
@@ -14,7 +25,7 @@ class RepositoriesTestCase(unittest.TestCase):
         # self.session = Session(engine)
         Session = sessionmaker(bind=engine, autocommit=True)
         self.session = Session()
-    
+
     # @unittest.skip("スキップ理由・・・")
     def test_add(self):
         with self.session.begin():
@@ -36,7 +47,7 @@ class RepositoriesTestCase(unittest.TestCase):
             user2 = User(name='jiro', fullname=u'テスト　次郎', password='jirojiro')
             user3 = User(name='sabu', fullname=u'テスト　三郎', password='sabusabu')
             addUser(self.session, [user1, user2, user3])
-        user = getUser(self.session, 'jiro')
+        user = getUser(self.session, name='jiro')
         self.assertEqual('jiro', user.name)
 
     def test_add_an_user_with_addresses(self):
@@ -47,6 +58,19 @@ class RepositoriesTestCase(unittest.TestCase):
                 ]
         with self.session.begin():
             addUser(self.session, [jack])
+            
+    def test_get_an_user_with_addresses(self):
+        jack = User(name='jack', fullname='Jack Bean', password='gjffdd')
+        jack.addresses = [
+                Address(email_address='jack@google.com'),
+                Address(email_address='j25@yahoo.com')
+                ]
+        with self.session.begin():
+            addUser(self.session, [jack])
+        user = getUser(self.session, name='jack')
+        
+        self.assertEqual('jack', user.name)
+        self.assertEqual(2, len(user.addresses))
 
 if __name__ == '__main__':
     unittest.main()
