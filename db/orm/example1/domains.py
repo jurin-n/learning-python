76ebd7a9139800ffa5fdf281d995636f3f84440b
jurin-n <jurin.nakano@gmail.com>
2016-11-13 11:32:57 +0900

    add Many to One relationship, from Addresses to Users.

--- db/orm/example1/domains.py
+++ db/orm/example1/domains.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
 
 class User(Base):
     __tablename__ = 'users'
@@ -13,7 +26,8 @@ class User(Base):
     fullname = Column(String)
     password = Column(String)
 
+    addresses = relationship("Address", order_by=Address.id, back_populates="user")
+
     def __repr__(self):
        return "<User(name='%s', fullname='%s', password='%s')>" % (
                             self.name, self.fullname, self.password)
-
