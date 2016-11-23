0129b0129cb601975e37ee634f4a466df72acea9
jurin-n <jurin.nakano@gmail.com>
2016-11-23 14:06:15 +0900

    delete using unicode().

--- effectivepython/item_03/sample_test.py
+++ effectivepython/item_03/sample_test.py
@@ -6,16 +6,16 @@ from sample import to_str, to_unicode
 class SampleTestCase(TestCase):
     # @skip('test')
     def test_to_str_change_unicode_to_str(self):
-        self.assertEqual(to_str(unicode(u'\u592a\u90ce')), '太郎')
+        self.assertEqual(to_str(u'\u592a\u90ce'), '太郎')
 
     def test_to_str_change_str_to_str(self):
         self.assertEqual(to_str('太郎'), '太郎')
 
     def test_to_unicode_change_str_to_unicode(self):
-        self.assertEqual(to_unicode('太郎'), unicode(u'\u592a\u90ce'))
+        self.assertEqual(to_unicode('太郎'), u'\u592a\u90ce')
         
     def test_to_unicode_change_unicode_to_unicode(self):
-        self.assertEqual(to_unicode(unicode(u'\u592a\u90ce')), unicode(u'\u592a\u90ce'))
+        self.assertEqual(to_unicode(u'\u592a\u90ce'), u'\u592a\u90ce')
 
 if __name__ == '__main__':
     main()
