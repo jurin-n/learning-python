3c67e6ea12aaf4b6261ec6b30d1693c1fbb8ecf2
jurin-n <jurin.nakano@gmail.com>
2016-11-30 22:04:05 +0900

    withターゲットのサンプルコード追加

--- effectivepython/item_43/sample.py
+++ effectivepython/item_43/sample.py
@@ -29,3 +29,24 @@ with debug_logging(logging.DEBUG):
 
 print('After')
 my_function()
+
+
+print('[INFO]Start with target *******************************:')
+
+@contextmanager
+def log_level(level,name):
+    logger = logging.getLogger(name)
+    old_level = logger.getEffectiveLevel()
+    logger.setLevel(level)
+    try:
+        yield logger
+    finally:
+        logger.setLevel(old_level)
+
+with log_level(logging.DEBUG,'my-log') as logger:
+    logger.debug('This is my message!')
+    logging.debug('This will not print')
+
+logger = logging.getLogger('my-log')
+logger.debug('Debug will not print')
+logger.error('Error will print')
