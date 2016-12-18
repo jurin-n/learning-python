2dd7e54d34873852f571345fbe632aca275e193a
jurin-n <jurin.nakano@gmail.com>
2016-12-18 14:51:24 +0900

    ディスクリプタとメタクラスを組み合わせた方法をためしてみた。

--- effectivepython/item_35/sample.py
+++ effectivepython/item_35/sample.py
@@ -25,3 +25,41 @@ foo = Customer()
 print('Before:', repr(foo.first_name), foo.__dict__, Customer.__dict__['first_name'])
 foo.first_name = 'test taro'
 print('After:', repr(foo.first_name), foo.__dict__, Customer.__dict__['first_name'])
+
+#
+# メタクラスを利用したバージョンに改善
+#
+class Field(object):
+    def __init__(self):
+        self.name = None
+        self.internal_name = None
+
+    def __get__(self, instance, instance_type):
+        if instance is None: return self
+        return getattr(instance, self.internal_name, '')
+    
+    def __set__(self, instance, value):
+        setattr(instance, self.internal_name, value)
+
+class Meta(type):
+    def __new__(meta, name, bases, class_dict):
+        for key, value in class_dict.items():
+            if isinstance(value, Field):
+                value.name = key
+                value.internal_name = '_' + key
+        cls = type.__new__(meta, name, bases, class_dict)
+        return cls
+
+class DatabaseRow(object):
+    __metaclass__ = Meta
+
+class BetterCustomer(DatabaseRow):
+    first_name = Field()
+    last_name = Field()
+    prefix = Field()
+    suffix = Field()
+
+foo = BetterCustomer()
+print('Before:', repr(foo.first_name), foo.__dict__, BetterCustomer.__dict__['first_name'])
+foo.first_name = 'test taro'
+print('After:', repr(foo.first_name), foo.__dict__, BetterCustomer.__dict__['first_name'])
