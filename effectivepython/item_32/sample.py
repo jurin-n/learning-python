25340791b1862a01f9c455753eba276c8f11269d
jurin-n <jurin.nakano@gmail.com>
2016-12-19 21:12:07 +0900

    __getattribute__をためした。

--- effectivepython/item_32/sample.py
+++ effectivepython/item_32/sample.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+# __getattr__試す
+#
 
 class LazyDB(object):
     def __init__(self):
@@ -24,3 +27,41 @@ data = LoggingLazyDB()
 print('exists:', data.exists)
 print('foo:   ', data.foo)
 print('foo:   ', data.foo)
+
+#
+# __getattribute__試す
+#
+class ValidatingDB(object):
+    def __init__(self):
+        self.exists = 5
+    
+    def __getattribute__(self, name):
+        print('Called __getattribute__(%s)' % name)
+        try:
+            # return super(self.__class__, self).__getattribute__(name)
+            return self.__getattribute__(name)
+        except AttributeError:
+            value = 'Value for %s' % name
+            setattr(self, name, value)
+            return value
+
+class ValidatingDB2(object):
+    def __init__(self):
+        self.exists = 5
+
+    def __getattribute__(self, name):
+        print('Called __getattribute__(%s)' % name)
+        try:
+            #return super().__getattribute__(name) # don't work on python 2.7.
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            print('AttributeError')
+            value = 'Value for %s' % name
+            setattr(self, name, value)
+            return value
+
+print('[INFO]dump ValidatingDB instance attr')
+data = ValidatingDB2()
+print('exits:', data.exists)
+print('foo:  ', data.foo)
+print('foo:  ', data.foo)
