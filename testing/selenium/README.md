c184c398d82d2eec1033ef73c48365c20ca15af3
jurin-n <jurin.nakano@gmail.com>
2017-09-18 17:37:27 +0900

    modify selenium sample & README.md.

--- testing/selenium/README.md
+++ testing/selenium/README.md
@@ -1,12 +1,25 @@
 ## 初期設定(自分の環境用) 
 . ~/development/virtualenv/venv27/bin/activate
 
+## 初期設定(自分の環境用２) 
+conda create -n selenium_py36 python=3.6
+source activate selenium_py36
+
 ## selenium関連モジュールインストール 
 pip install selenium
 
 ## ChromeDriver
-https://sites.google.com/a/chromium.org/chromedriver/
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+## Geckodriver
+https://github.com/mozilla/geckodriver/releases
 
 ## 実行
-python sample.py {PATH_TO_CHROME_DRIVER} {PATH_TO_SCLEENSHOT_STORE}
- 実行例)python sample.py ~/development/selenium/chromedriver ~
+### 初期設定(driverへのパスを通す)
+export PATH=$PATH:~/development/selenium
+
+### Chrome Driverで実行の場合
+python sample.py Chrome
+
+### Firefox Driverで実行の場合
+python sample.py Firefox
--- testing/selenium/sample.py
+++ testing/selenium/sample.py
@@ -2,17 +2,42 @@
 
 from selenium import webdriver
 import sys
+import os
 
-path_to_driver = sys.argv[1] # Chrome Driverへの絶対パス
-path_to_screenshot = sys.argv[2] # スクリーンショット格納場所の絶対パス
+driver_name = sys.argv[1] # Driver選択用
 
 # driver = webdriver.Firefox()
 # Optional argument, if not specified will search path.
 # driver = webdriver.Chrome('/path/to/chromedriver')
-driver = webdriver.Chrome(path_to_driver)
+if driver_name =='Firefox':
+    print('seleted Firefox Driver.')
+    driver = webdriver.Firefox()
+elif driver_name =='Chrome':
+    print('seleted Chrome Driver.')
+    driver = webdriver.Chrome()
+else:
+    raise Exception()
 
-driver.get('http://example.selenium.jp/reserveApp')
+dir_for_screenshot = os.path.join(os.getcwd(), 'screen-shot')
+dir_for_html_source = os.path.join(os.getcwd(), 'html-source')
+
+if not os.path.exists(dir_for_screenshot):
+    os.makedirs(dir_for_screenshot)
+
+if not os.path.exists(dir_for_html_source):
+    os.makedirs(dir_for_html_source)
+
+
+screenshot_file = os.path.join(dir_for_screenshot, 'foo'+ '_'+ driver_name +'.png')
+html_source_file = os.path.join(dir_for_html_source, 'foo'+ '_'+ driver_name +'.html')
+
+driver.get('http://example.selenium.jp/reserveApp_Renewal/')
 driver.find_element_by_id('guestname').send_keys(u'サンプルユーザ')
-driver.get_screenshot_as_file(path_to_screenshot + '/foo.png')
-driver.find_element_by_id('goto_next').click()
+driver.save_screenshot(screenshot_file)
+
+page_source_file = open(html_source_file,'wb')
+page_source_file.write(driver.page_source.encode())
+page_source_file.close()
+
+driver.find_element_by_id('agree_and_goto_next').click()
 driver.quit()
