ca9a03f6f3106f62b6cb367f441a28a1903d0173
jurin-n <jurin.nakano@gmail.com>
2019-12-04 21:32:25 +0900

    isoformatにasia/tokyoタイムゾーン追加しようと思ったけど分からなかったので、isoformat諦めた。

--- basic/directories_files_listing3.py
+++ basic/directories_files_listing3.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import sys
 import csv
 import datetime
+from datetime import timezone
+from datetime import timedelta
 
 BASE_PATH=sys.argv[1]
 
@@ -23,6 +25,10 @@ with open('file_list.csv', 'w', newline='', encoding='utf-8') as csvfile:
          #print(f.stat())
          #print(datetime.datetime.fromtimestamp(f.stat().st_mtime).isoformat())
          #print(str(f.is_file())+":"+ str(f.is_symlink())+":"+f.suffix + ":"  + f.name + ":" + str(f))
-         mtime=datetime.datetime.fromtimestamp(f.stat().st_mtime).isoformat()
+         d=datetime.datetime.fromtimestamp(f.stat().st_mtime)
+         #print(type(d.timezone(timedelta(hours=9))))
+         #mtime=d.isoformat()
+         mtime=str(d)        
+ 
          row = {'full_path':str(f), 'file_name':f.name, 'suffix':f.suffix ,'is_file':f.is_file(), 'is_symlink':f.is_symlink(), 'mtime':mtime}
          writer.writerow(row)
